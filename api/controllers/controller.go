61cb53d3272ef0bc0fae2b97de8a12868e5dd33f
Dennis Boachie Boateng <dennisboachie9@gmail.com>
2024-06-07 19:56:55 +0000

    add netlify app

--- api/controllers/controller.go
+++ api/controllers/controller.go
@@ -20,7 +20,6 @@ func NewController(store *db.Store, router *gin.Engine) *Controller {
 func (c *Controller) ConfigCORSMiddleWare() {
 	c.Router.Use(cors.New(cors.Config{
 		AllowOrigins: []string{"http://localhost:5173",
-			"https://routing-web.vercel.app",
 			"https://findmyclassroomknust.netlify.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
