53edbd6058756840b3136ba7e74a8287a4bd8bd1
Dennis Boachie Boateng <dennisboachie9@gmail.com>
2024-07-21 16:49:04 +0000

    configured cors

--- api/controllers/controller.go
+++ api/controllers/controller.go
@@ -20,7 +20,9 @@ func NewController(store *db.Store, router *gin.Engine) *Controller {
 func (c *Controller) ConfigCORSMiddleWare() {
 	c.Router.Use(cors.New(cors.Config{
 		AllowOrigins: []string{"http://localhost:5173",
-			"https://findmyclassroomknust.netlify.app"},
+			"https://findmyclassroomknust.netlify.app",
+			"https://london-navigator.netlify.app",
+		},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
